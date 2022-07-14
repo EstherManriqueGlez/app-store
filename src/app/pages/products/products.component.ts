0e47b43c12f040cf679b69128103772cba32c2d9
Esther Manrique <esther_manrique@epam.com>
2022-07-14 09:56:11 -0500

    Added products-component, service and angular material

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { Product } from './interfaces/product.interface';
+import { ProductsService } from './services/products.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  products!: Product[];
+
+  constructor(private productService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts()
+    .pipe(
+      tap((products: Product[]) => this.products = products)
+    )
+    .subscribe();
+  }
+
+}
